5bb2b1f30785fabb04ec2ac7ef7212a80db5bf16
Abej-Rijwi <abejrijwicareer@gmail.com>
2025-06-21 04:29:11 +0530

    first commit

--- next.config.ts
+++ next.config.ts
@@ -17,10 +17,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  experimental: {
-    ppr: "incremental",
-    after: true,
-  },
   devIndicators: {
     appIsrStatus: true,
     buildActivity: true,
